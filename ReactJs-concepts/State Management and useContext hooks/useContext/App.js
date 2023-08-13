853a0fe28212ac046a7d2a2e991ff2d285962066
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-13 19:20:49 +0530

    added files via upload

--- ReactJs-concepts/State Management and useContext hooks/useContext/App.js
+++ ReactJs-concepts/State Management and useContext hooks/useContext/App.js
@@ -1,1 +1,42 @@
+import { useEffect, useState, createContext} from "react";
+import Axios from "axios";
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom'
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
+
+
+export const AppContext = createContext();
+
+
+
+function App() {
+  
+  const [userName, setUserName] = useState("Naman")
+
+  return (
+    <div className="App">
+
+      <AppContext.Provider value={{userName, setUserName}}>
+      <Router>
+
+        <Navbar/>
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="*" element={<h1>Page not found</h1>}/>
+        </Routes>
+      </Router>
+
+
+      </AppContext.Provider>
+    </div>
+
+  );
+}
+
+export default App;
 
