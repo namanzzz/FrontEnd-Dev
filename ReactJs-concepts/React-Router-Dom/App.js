db49a44497cd52c2ab6a264a70771d8fa4d9419b
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-13 17:07:44 +0530

    Update App.js

--- ReactJs-concepts/React-Router-Dom/App.js
+++ ReactJs-concepts/React-Router-Dom/App.js
@@ -1,1 +1,33 @@
 
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom'
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
+
+function App() {
+  
+  
+
+  return (
+    <div className="App">
+
+      <Router>
+
+        <Navbar/>
+
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/menu" element={<Menu/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="*" element={<h1>Page not found</h1>}/>
+        </Routes>
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
