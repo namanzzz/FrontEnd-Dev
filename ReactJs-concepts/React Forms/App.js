4751f0eb9efc4c79507f73a19f56b0f53c47d35c
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-14 18:52:17 +0530

    add files via upload

--- ReactJs-concepts/React Forms/App.js
+++ ReactJs-concepts/React Forms/App.js
@@ -1,1 +1,20 @@
+import { Form } from './pages/Form'
+
+
+function App() {
+  
+ 
+
+  return (
+    <div className="App">
+     
+     
+     <Form/>
+      
+    </div>
+
+  );
+}
+
+export default App;
 
