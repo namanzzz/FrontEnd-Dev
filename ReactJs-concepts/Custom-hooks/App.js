26eccbd65e0d43282bef3bf53a2aa4db571a4818
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-17 16:20:07 +0530

    add files via upload

--- ReactJs-concepts/Custom-hooks/App.js
+++ ReactJs-concepts/Custom-hooks/App.js
@@ -1,1 +1,31 @@
 
+// import {useState} from 'react'
+import { useToggle } from './useToggle';
+
+function App() {
+  
+    const [isVisible, toggle] = useToggle();
+    const [isVisible2, toggle2] = useToggle();
+  return (
+    <div className="App">
+     <button onClick={toggle}>
+      {isVisible ? "Hide": "Show"}
+     </button>
+     
+     {isVisible && <h1>Hidden Text</h1>}
+
+
+
+      <button onClick={toggle2}>
+        {isVisible2 ? "Hide" : "Show"}
+      </button>
+
+      {isVisible2 && <h1>Hidden Text</h1>}
+     
+      
+    </div>
+
+  );
+}
+
+export default App;
