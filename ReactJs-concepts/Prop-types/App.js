86c06149bc18c14368628eb64d046945cfb90443
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-21 16:42:28 +0530

    Update App.js

--- ReactJs-concepts/Prop-types/App.js
+++ ReactJs-concepts/Prop-types/App.js
@@ -1,1 +1,26 @@
+// import {useState} from 'react'
+import { useToggle } from './useToggle';
+import { Person } from './components/person';
+
+function App() {
+  
+  return (
+    <div className="App">
+
+
+     <Person
+     name={21}
+     email="Naman@gmail.com"
+     age={21}
+     isMarried={false}
+     friends={["se", "hiu", "ansh", "me"]}
+     />
+     
+      
+    </div>
+
+  );
+}
+
+export default App;
 
