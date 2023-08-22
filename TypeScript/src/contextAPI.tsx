30b3790f553964da36e951447544e335f0ca2f31
Naman <60322358+namanzzz@users.noreply.github.com>
2023-08-22 16:39:42 +0530

    add files via upload

--- TypeScript/src/contextAPI.tsx
+++ TypeScript/src/contextAPI.tsx
@@ -1,1 +1,31 @@
+import React, {FC, createContext} from 'react';
+import './App.css';
+import { Person, HairColor } from './components/Person';
+
+
+interface AppContextInterface{
+  name: string;
+  age: number;
+  country: string
+}
+
+
+const AppContext = createContext<AppContextInterface | null>(null)
+
+const App: FC = () => {
+  const contextValue: AppContextInterface = {
+    name: "Naman",
+    age: 20,
+    country: "India"
+  }
+  return (
+    <AppContext.Provider value={contextValue}>
+    <div className="App">
+         <Person name="Naman" age={21} email="namansharma50962@gmail.com" hairColor={HairColor.Blonde}/>
+    </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
 
